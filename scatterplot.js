2037c941a918f84d83c5ed37fa099c0b39afa3f6
riquemg <83304893+riquemg@users.noreply.github.com>
2023-06-15 18:46:28 -0300

    Add files via upload

--- scatterplot.js
+++ scatterplot.js
@@ -1,14 +1,14 @@
 ﻿// Carrega os dados do arquivo CSV usando D3.js
-function plot(x, y, s){
+function scatterplot(x, y, s){
   d3.csv('classes_metrics.csv').then(data => {
     const axis_x = data.map(d => parseFloat(d[x]));
     const axis_y = data.map(d => parseFloat(d[y]));
     const godClassData = data.map(d => parseInt(d[s]));
 
     // Prepara os dados para o gráfico
-    const chartData = axis_x.map((_, index) => ({
-      x: axis_y[index],
-      y: axis_x[index],
+    const chartData = axis_y.map((_, index) => ({
+      x: axis_x[index],
+      y: axis_y[index],
       godClass: godClassData[index]
     }));
 
@@ -29,11 +29,11 @@ function plot(x, y, s){
 
     // Cria as escalas para os eixos x e y
     const xScale = d3.scaleLinear()
-      .domain([d3.min(axis_y), d3.max(axis_y)])
+      .domain([d3.min(axis_x), d3.max(axis_x)])
       .range([padding, width - padding]);
 
     const yScale = d3.scaleLinear()
-      .domain([d3.min(axis_x), d3.max(axis_x)])
+      .domain([d3.min(axis_y), d3.max(axis_y)])
       .range([height - padding, padding]);
 
     // Cria os pontos do gráfico
